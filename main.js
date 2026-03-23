e61b6669de5ab72f21f54bd809b2f4c5b57b2bb1
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-23 18:08:20 +0530

    createGrid function to create the required cell

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ sliderOutput.textContent = `${slider.value} x ${slider.value}`;
 slider.oninput = function() {
     sliderOutput.textContent = `${this.value} x ${this.value}`;
 }
+
+const container = document.getElementById("container");
+ 
+function createGrid(side) {
+    for (let i = 1; i <= side*side; i++){
+        let cell = document.createElement("div");
+        cell.className = 'grid-item';
+        cell.textContent = "Sample text";
+        container.append(cell);
+    }
+};
