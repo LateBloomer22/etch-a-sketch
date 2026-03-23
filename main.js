eb5345215510d56c0cc2314fc8b9896c2568b635
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-23 19:16:10 +0530

    Added random coloring through background-color

--- main.js
+++ main.js
@@ -16,8 +16,28 @@ function createGrid(side) {
     for (let i = 1; i <= side*side; i++){
         let cell = document.createElement("div");
         cell.className = 'grid-item';
-        cell.textContent = "Sample text";
+        // cell.textContent = "text";
         container.append(cell);
     }
+    applyStyle();
 };
+
+function applyStyle() {
+    let elements = document.querySelectorAll('.grid-item');
+    elements.forEach(el => {
+        Object.assign (el.style, {
+            width: `calc(600px/${slider.value})`,
+            height: `calc(600px/${slider.value})`,
+            border: `0.5px solid grey`
+        });
+        el.addEventListener("mouseover", () => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+            el.style["background-color"] = `rgb(${r},${g},${b})`;
+        })
+    });
+}
+
+
 
