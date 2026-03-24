4fbda16bb0a66ae57e29e20f66f80629e51dd538
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-24 14:19:23 +0530

    Added darkening option

--- main.js
+++ main.js
@@ -2,45 +2,47 @@ let slider = document.getElementById("input");
 let sliderOutput = document.getElementById("sliderOutput");
 const container = document.getElementById("container");
 let eraserBtn = document.getElementById("eraser");
+let darkenBtn = document.getElementById("darken");
 let isOn = false;
-
+let isDarken = false;
 
 sliderOutput.textContent = `${slider.value} x ${slider.value}`;
 createGrid(slider.value);
+applyStyle();
 
 // Slider input for user
 slider.oninput = function() {
-    reset();
+    container.innerHTML = "";
+    resetDarken();
+    resetEraser();
     sliderOutput.textContent = `${this.value} x ${this.value}`;
     createGrid(this.value);
-    
+    applyStyle();
 }
 
 // Eraser button for user
 eraserBtn.addEventListener("click", () => {
     isOn = !isOn;
-    eraserBtn.style["backgroundColor"] = isOn ? "#007FFF" : "white";
-    if(isOn){
-        let elements = document.querySelectorAll('.grid-item');
-        elements.forEach(el => {
-            el.addEventListener("mouseover", () => {
-                eraser(el);
-            });
-        });
-    } 
-    else {
-        applyStyle();
-    };
+    eraserBtn.style["backgroundColor"] = isOn ? "#007FFF" : "rgb(255,255,255)";
+    resetDarken();
+    applyStyle();
 });
 
+// Darken button to increase darkness
+darkenBtn.addEventListener("click", () => {
+    isDarken = !isDarken;
+    darkenBtn.style["backgroundColor"] = isDarken ? "#007FFF" : "rgb(255,255,255)";
+    resetEraser();
+    applyStyle();
+});
 
 function createGrid(side) {
     for (let i = 1; i <= side*side; i++){
         let cell = document.createElement("div");
         cell.className = 'grid-item';
+        cell.style["backgroundColor"] = "rgba(255,255,255,1)";
         container.append(cell);
     }
-    applyStyle();
 };
 
 function applyStyle() {
@@ -52,7 +54,13 @@ function applyStyle() {
             border: `0.5px solid grey`
         });
         el.addEventListener("mouseover", () => {
-            applyBackground(el);            
+            if (isOn) {
+                eraser(el);
+            } else if (isDarken) {
+                darken(el, 0.9);
+            } else {
+                applyBackground(el);
+            }
         })
     });
 }
@@ -65,17 +73,24 @@ function applyBackground (elem) {
 }
 
 function eraser(elem) {
-    elem.style["background-color"] = "white";
+    elem.style["background-color"] = "rgb(255,255,255)";
 }
 
-function reset() {
-    container.innerHTML = "";
+function resetEraser() {
     isOn = false;
-    eraserBtn.style["backgroundColor"] = "white";
+    eraserBtn.style["backgroundColor"] = "rgb(255,255,255)";
 }
 
+function resetDarken() {
+    isDarken = false;
+    darkenBtn.style["backgroundColor"] = "rgb(255,255,255)";
+}
 
-
-
-
-
+function darken(elem, amount) {
+    let bgColor = window.getComputedStyle(elem).backgroundColor;
+    let [r, g, b] = bgColor.match(/\d+/g).map(Number);
+    r = Math.floor(r * amount);
+    g = Math.floor(g * amount);
+    b = Math.floor(b * amount);
+    elem.style["background-color"] = `rgb(${r},${g},${b})`;
+}
